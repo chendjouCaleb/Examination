755040800931a5d0a71eee8d8098e8d777fa29d1
Chendjou deGrace <chendjou2016@outlook.fr>
2020-04-23 17:19:33 +0100

    Admin delete

--- ClientApp/src/app/organisation/admin/list/admin-list.page.ts
+++ ClientApp/src/app/organisation/admin/list/admin-list.page.ts
@@ -1,10 +1,12 @@
 ﻿import {Component, OnInit} from "@angular/core";
-import {Admin, AdminLoader, Organisation} from "src/models";
+import {Admin, AdminHttpClient, AdminLoader, Organisation} from "src/models";
 import {CurrentItems} from "src/app/current-items";
 import {List} from "@positon/collections";
 import {MatDialog} from "@angular/material/dialog";
 import {AdminAddComponent} from "../add/admin-add.component";
 import {AdminEditComponent} from "../edit/admin-edit.component";
+import {Confirmation} from "../../../../controls/confirmation/confirmation";
+import {AlertEmitter} from "../../../../controls/alert-emitter";
 
 @Component({
   templateUrl: 'admin-list.page.html',
@@ -15,7 +17,11 @@ export class AdminListPage implements OnInit {
   organisation: Organisation;
   admins: List<Admin>;
 
-  constructor(private currentItems: CurrentItems, private _adminLoader: AdminLoader, private _dialog: MatDialog) {
+  constructor(private currentItems: CurrentItems, private _adminLoader: AdminLoader,
+              private _httpClient: AdminHttpClient,
+              private _alertEmitter: AlertEmitter,
+              private _confirmation: Confirmation,
+              private _dialog: MatDialog) {
     this.organisation = currentItems.get('organisation');
   }
 
@@ -36,5 +42,14 @@ export class AdminListPage implements OnInit {
   openEditDialog(admin: Admin) {
     const modalRef = this._dialog.open(AdminEditComponent );
     modalRef.componentInstance.admin = admin;
+  }
+
+  delete(admin: Admin) {
+    const result = this._confirmation.open("Voulez-vous Supprimer cet administrateur?");
+    result.accept.subscribe(async () => {
+      await this._httpClient.delete(admin.id);
+      this.admins.remove(admin);
+      this._alertEmitter.info("L'administrateur a été supprimé!")
+    })
   }
 }
--- ClientApp/src/controls/control.module.ts
+++ ClientApp/src/controls/control.module.ts
@@ -6,12 +6,15 @@ import {Labelled} from "./labelled/labelled";
 import {FloatButton} from "./float-button/float-button";
 import {ControlErrorComponent} from "./form/control.error.component";
 import {AlertEmitter} from "./alert-emitter";
+import {ConfirmationComponent} from "./confirmation/confirmation.component";
+import {MatDialogModule} from "@angular/material/dialog";
+import {Confirmation} from "./confirmation/confirmation";
 
 @NgModule({
-  imports: [ CommonModule, MsfButtonModule],
-  providers: [ AlertEmitter ],
-  declarations: [ LabelledIcon, Labelled, FloatButton, ControlErrorComponent ],
-  exports: [ LabelledIcon, Labelled, FloatButton, ControlErrorComponent ],
+  imports: [ CommonModule, MatDialogModule, MsfButtonModule],
+  providers: [ AlertEmitter, Confirmation ],
+  declarations: [ LabelledIcon, Labelled, FloatButton, ControlErrorComponent, ConfirmationComponent ],
+  exports: [ LabelledIcon, Labelled, FloatButton, ControlErrorComponent, ConfirmationComponent ],
 })
 export class ControlModule {
 
